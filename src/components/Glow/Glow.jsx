ba325d957b37f164a5cec2f08f297aa8b142f1c3
Boye <samuel.boye95@yahoo.com>
2022-08-02 22:27:51 +0100

    add working on glow section

--- src/components/Glow/Glow.jsx
+++ src/components/Glow/Glow.jsx
@@ -1,11 +1,31 @@
 import './Glow.scss'
+import GlowImg from '../../assets/glow/glow.png'
+import Rinse from '../../assets/glow/rinse.png'
+import Wash from '../../assets/glow/wash.png'
+import Toner from '../../assets/glow/toner.png'
 
 const Glow = () => {
-    return(
-        <section>
-            wdwgd
-        </section>
-    )
+  return (
+    <section className='container glow'>
+      <img src={GlowImg} alt='glow' className='glow__img' />
+      <div className='glow__content'>
+        <h2 className='glow__title'>THREE STEPS GLOW</h2>
+        <div className='glow__text'>
+            <img src={Wash} alt="wash" />
+            <p>Deep wash your face</p>
+        </div>
+        <div className='glow__text'>
+            <img src={Toner} alt="toner" />
+            <p>Apply some of our toner on your face</p>
+        </div>
+        <div className='glow__text'>
+            <img src={Rinse} alt="rinse" />
+            <p>Rinse face after 15mins</p>
+        </div>
+        <button>Learn More</button>
+      </div>
+    </section>
+  )
 }
 
 export default Glow
--- src/components/Toner/Toner.jsx
+++ src/components/Toner/Toner.jsx
@@ -1,5 +1,7 @@
 import './Toner.scss'
 import ToneImg from '../../assets/toner/maxitoner.png'
+import Toner1 from '../../assets/toner/toner1.png'
+import Toner2 from '../../assets/toner/toner2.png'
 
 const Toner = () => {
     return(
@@ -14,7 +16,9 @@ const Toner = () => {
                     <li>Apply some our toner to  <br />your face</li>
                 </div>    
                 <div className='toner__image'>
-                    <img src={ToneImg} alt='toner' />
+                    <img src={Toner1} alt1='toner1' className='toneI' />
+                    <img src={ToneImg} alt='toner' className='toneImg' />
+                    <img src={Toner2} alt2='toner2' className='toneI' />
                 </div>
                 <div className='toner__words'>
                     <li>Soft toner foam stimulates  <br />your face</li>
