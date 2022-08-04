fa1e3bb7b5d28cac3c494fed5d3c6ffa92360e8a
Boye <samuel.boye95@yahoo.com>
2022-08-04 23:38:42 +0100

    add done with review section not responsive

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,9 +1,30 @@
 import './Review.scss'
 import Reviewer from '../../assets/reviews/reviewer.png'
+import { Pagination, Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react'
-
-// Import Swiper styles
 import 'swiper/scss'
+import 'swiper/scss/pagination'
+
+const data = [
+  {
+    avatar: Reviewer,
+    name: 'Juliet Adekunle',
+    review:
+      "One of the best brands I've ever used. The cream toner does magic to my skin. I was battling with acute acne before my friend recommended this brand. XOXO"
+  },
+  {
+    avatar: Reviewer,
+    name: 'Juliet Adekunle',
+    review:
+      "One of the best brands I've ever used. The cream toner does magic to my skin. I was battling with acute acne before my friend recommended this brand. XOXO"
+  },
+  {
+    avatar: Reviewer,
+    name: 'Juliet Adekunle',
+    review:
+      "One of the best brands I've ever used. The cream toner does magic to my skin. I was battling with acute acne before my friend recommended this brand. XOXO"
+  }
+]
 
 const Review = () => {
   return (
@@ -12,23 +33,33 @@ const Review = () => {
         WHAT OUR DOLLS ARE SAYING <br />
         ABOUT OUR PRODUCTS
       </div>
-      <Swiper spaceBetween={50} slidesPerView={3}>
-        <SwiperSlide>
-          <div className='review__reviews'>
-            <div className='review__image'>
-              <img src={Reviewer} alt='reviewer' />
-            </div>
-            <div className='reviews'>
-              <p>
-                One of the best brands i've ever used. The cream toner does
-                magic to my skin. I was battling with acute acne before my
-                friend recommeded this brand. XOXO
-              </p>
-              <h6 className='reviewer__name'>Juliet Adekunle</h6>
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      <div className='swiper__qoute'>
+        <Swiper
+          className='swiper__box'
+          modules={[Pagination, Autoplay]}
+          spaceBetween={40}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false
+          }}
+          slidesPerView={1}
+          pagination={true}
+        >
+          {data.map(({ avatar, name, review }, index) => {
+            return (
+              <SwiperSlide key={index} className='review__reviews'>
+                <div className='review__image'>
+                  <img src={avatar} alt='reviewer' />
+                </div>
+                <div className='reviews'>
+                  <p>{review}</p>
+                  <h6 className='reviewer__name'>{name}</h6>
+                </div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
     </section>
   )
 }
