2e61495c5b5073a3fdcd7e02818b0e75a5a9b4a1
Boye <samuel.boye95@yahoo.com>
2022-08-07 00:02:22 +0100

    add working on discount section

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -1,11 +1,40 @@
 import './Discount.scss'
+import discount1 from '../../assets/discount/1.png'
+import discount2 from '../../assets/discount/2.png'
 
 const Discount = () => {
-    return(
-        <section>
-            wdwgd
-        </section>
-    )
+  return (
+    <section className='container discount'>
+      <div className='discountbox box1'>
+        <div className='discounts__image'>
+          <img src={discount1} alt='discount' />
+        </div>
+        <div className='discounts__info'>
+          <h5 className='discount__header'>GET 20% DISCOUNT OFF COMBO PACK</h5>
+          <p className='discount__text'>
+            Lorem Ipsum is simply dummy text of the nd typesettley of type and
+            scrambled it to make a type specimen book. It has survidus PageMaker
+            including versions of Lorem Ipsum.
+          </p>
+          <button>Buy Now</button>
+        </div>
+      </div>
+      <div className='discountbox box2'>
+        <div className='discounts__image'>
+          <img src={discount2} alt='discount' />
+        </div>
+        <div className='discounts__info'>
+          <h5 className='discount__header'>GET 20% DISCOUNT OFF COMBO PACK</h5>
+          <p className='discount__text'>
+            Lorem Ipsum is simply dummy text of the nd typesettley of type and
+            scrambled it to make a type specimen book. It has survidus PageMaker
+            including versions of Lorem Ipsum.
+          </p>
+          <button>Buy Now</button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Discount
