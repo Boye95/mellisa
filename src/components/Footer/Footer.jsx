d35ccd16b1a70752188217a8845984336171bd8f
Boye <samuel.boye95@yahoo.com>
2022-08-08 14:59:13 +0100

    add working footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,29 @@
 import './Footer.scss'
 
+import location from '../../assets/footer/location1.svg'
+import mail from '../../assets/footer/mail1.svg'
+import phone from '../../assets/footer/phone.svg'
+
 const Footer = () => {
-    return(
-        <section>
-            wdwgd
-        </section>
-    )
+  return (
+    <section className='container footer'>
+      <div className='footer__contact'>
+        <p>
+          <img src={phone} alt='phone' />
+          <a href='tel:+44799992972282'>++447 9999 2972 282</a>
+        </p>
+        <p>
+            <img src={mail} alt="mail" />
+            <a href="mailto:himellisa@gmail.com">himellisa@gmail.com</a>
+        </p>
+        <p>
+            <img src={location} alt="location" />
+            <a href="mailto:himellisa@gmail.com">38a Lucas way,England</a>
+        </p>
+
+      </div>
+    </section>
+  )
 }
 
 export default Footer
--- src/components/Skin/Skin.jsx
+++ src/components/Skin/Skin.jsx
@@ -1,11 +1,29 @@
 import './Skin.scss'
 
+import Skin1 from '../../assets/skins/1.png'
+import Skin2 from '../../assets/skins/2.png'
+import Skin3 from '../../assets/skins/3.png'
+import Skin4 from '../../assets/skins/4.png'
+import Skin5 from '../../assets/skins/5.png'
+
+
 const Skin = () => {
-    return(
-        <section>
-            wdwgd
-        </section>
-    )
+  return (
+    <section className='container skin'>
+      <div className='skin__header'>
+        <h1>FOR ALL SKIN TYPE</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and i</p>
+      </div>
+
+      <div className="skin__reel">
+        <img src={Skin1} alt="skin" className="skins" />
+        <img src={Skin2} alt="skin" className="skins" />
+        <img src={Skin3} alt="skin" className="skins" />
+        <img src={Skin4} alt="skin" className="skins" />
+        <img src={Skin5} alt="skin" className="skins" />
+      </div>
+    </section>
+  )
 }
 
 export default Skin
