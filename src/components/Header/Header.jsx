1f462800b743e1d93e905c09919f81e41648a88a
Boye <samuel.boye95@yahoo.com>
2022-08-13 06:06:23 +0100

    add hero section mobile(600px) styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,53 +1,55 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './Header.scss'
 import ham from '../../assets/nav/ham.png'
 import x from '../../assets/nav/x.png'
 import arrow from '../../assets/nav/arrow.png'
 
-
 const Header = () => {
-  
-// Navbar display script
-let [showMenu, isShowMenu] = useState(false)
+  // Navbar display script
+  let [showMenu, setShowMenu] = useState(false)
+  let ref = useRef()
 
-  const body = document.querySelector('body')
-  // const open = document.querySelector('.ham')
-  const navResponsive = document.querySelector('.ham__container')
-  // const closeBtn = document.querySelector('.x')
-  // const overlay = document.querySelector('.overlay')
-  let mQuery900 = window.matchMedia('(max-width: 900px)')
-  
-  let menuOpen = false
   
   function handleShowMenu () {
-    menuOpen = true
-    body.style.overflow = 'hidden'
-    navResponsive.style.right = '0'
-  
-    if (mQuery900.matches) {
-      navResponsive.style.width = '100vw'
-    } else {
-      navResponsive.style.width = '40vw'
-      // overlay.style.display = 'block'
-    }
+    setShowMenu(!showMenu)
+    ref.current.style.right = '0'
   }
-  
+
   function handleHideMenu () {
-    menuOpen = false
-  
-    navResponsive.style.width = '0'
-    navResponsive.style.right = '-40vw'
-    body.style.overflow = 'auto'
-    // overlay.style.display = 'none'
+    setShowMenu(!showMenu)
+    ref.current.style.width = '0'
+    ref.current.style.right = '-40vw'
   }
-  
-  // open.addEventListener('click', showMenu)
-  // closeBtn.addEventListener('click', hideMenu)
-  mQuery900.addEventListener('change', () => {
-    if (menuOpen) {
-      handleShowMenu()
-    }
-  })
+
+  let hamNav = () => {
+    return (
+      <div ref={ref} className='ham__container'>
+        <div className='x'>
+          <img src={x} alt='xx' onClick={handleHideMenu} />
+        </div>
+        <a href='#' className='link'>
+          Home
+          <img src={arrow} alt='arrow' />
+        </a>
+        <a href='#' className='link'>
+          About Us
+          <img src={arrow} alt='arrow' />
+        </a>
+        <a href='#' className='link'>
+          How it works
+          <img src={arrow} alt='arrow' />
+        </a>
+        <a href='#' className='link'>
+          Login
+          <img src={arrow} alt='arrow' />
+        </a>
+        <a href='#' className='link sign'>
+          Sign Up
+          <img src={arrow} alt='arrow' />
+        </a>
+      </div>
+    )
+  }
 
   return (
     <header className='container nav'>
@@ -74,31 +76,7 @@ let [showMenu, isShowMenu] = useState(false)
         <img src={ham} alt='ham' className='ham' onClick={handleShowMenu} />
       </div>
 
-      <div className='ham__container'>
-        <div className='x'>
-          <img src={x} alt='xx' onClick={handleHideMenu} />
-        </div>
-        <a href='#' className='link'>
-          Home
-          <img src={arrow} alt='arrow' />
-        </a>
-        <a href='#' className='link'>
-          About Us
-          <img src={arrow} alt='arrow' />
-        </a>
-        <a href='#' className='link'>
-          How it works
-          <img src={arrow} alt='arrow' />
-        </a>
-        <a href='#' className='link'>
-          Login
-          <img src={arrow} alt='arrow' />
-        </a>
-        <a href='#' className='link sign'>
-          Sign Up
-          <img src={arrow} alt='arrow' />
-        </a>
-      </div>
+      {showMenu ? hamNav() : null}
     </header>
   )
 }
