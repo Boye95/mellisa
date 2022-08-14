3f3d1d5c0e24ae1d3ff7125cbefdb3a222cfb1d3
Boye <samuel.boye95@yahoo.com>
2022-08-14 21:50:31 +0100

    add styled discount & skin section 600px

--- src/components/Products/MobileProducts.jsx
+++ src/components/Products/MobileProducts.jsx
@@ -1,53 +1,27 @@
 import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
 import { FreeMode, Pagination } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/scss'
 import 'swiper/scss/free-mode'
 import 'swiper/scss/pagination'
 
 
-
 export default function MobileProducts ({ data1, data2 }) {
-
-  let slidePikin = data1.map(({ prodImg, name, price }, index) => {
-    return (
-      <div key={index} className='product__product'>
-        <div className='products__image'>
-          <img src={prodImg} alt='product' />
-        </div>
-        <div className='products__info'>
-          <h5 className='products__name'>{name}</h5>
-          <p className='products__price'>{price}</p>
-        </div>
-      </div>
-    )
-  })
-  
   return (
     <div>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        pagination={{
-          clickable: true
-        }}
-        modules={[FreeMode, Pagination]}
-        className='mobile__product'
-        shouldSwiperUpdate
-      >
-        {slidePikin}
-      </Swiper>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        freeMode={true}
-        modules={FreeMode}
-        className='mobile__product'
-      >
-        {data2.map(({ prodImg, name, price }, index) => {
+    <Swiper
+      slidesPerView={3}
+      spaceBetween={30}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="mySwiper"
+    >
+      {data1.map(({ prodImg, name, price }, index) => {
           return (
-            <SwiperSlide className='product__product' key={index}>
+            <SwiperSlide className='products__product' key={index}>
               <div className='products__image'>
                 <img src={prodImg} alt='product' />
               </div>
@@ -56,10 +30,11 @@ export default function MobileProducts ({ data1, data2 }) {
                 <p className='products__price'>{price}</p>
               </div>
             </SwiperSlide>
-            
           )
         })}
-      </Swiper>
+        <SwiperSlide>ffsss</SwiperSlide>
+    </Swiper>
+
     </div>
   )
 }
--- src/components/Skin/Skin.jsx
+++ src/components/Skin/Skin.jsx
@@ -1,11 +1,15 @@
 import './Skin.scss'
+import { FreeMode } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/scss'
+import 'swiper/scss/free-mode'
+import 'swiper/scss/pagination'
 
 import Skin1 from '../../assets/skins/1.png'
 import Skin2 from '../../assets/skins/2.png'
 import Skin3 from '../../assets/skins/3.png'
 import Skin4 from '../../assets/skins/4.png'
 import Skin5 from '../../assets/skins/5.png'
-
 
 const Skin = () => {
   return (
@@ -15,12 +19,38 @@ const Skin = () => {
         <p>Lorem Ipsum is simply dummy text of the printing and i</p>
       </div>
 
-      <div className="skin__reel">
-        <img src={Skin1} alt="skin" className="skins" />
-        <img src={Skin2} alt="skin" className="skins" />
-        <img src={Skin3} alt="skin" className="skins" />
-        <img src={Skin4} alt="skin" className="skins" />
-        <img src={Skin5} alt="skin" className="skins" />
+      <div className='skin__reel'>
+        <img src={Skin1} alt='skin' className='skins' />
+        <img src={Skin2} alt='skin' className='skins' />
+        <img src={Skin3} alt='skin' className='skins' />
+        <img src={Skin4} alt='skin' className='skins' />
+        <img src={Skin5} alt='skin' className='skins' />
+      </div>
+
+      <div className='skin__reelmobile'>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          freeMode={true}
+          modules={[FreeMode]}
+          className='mySwiper'
+        >
+          <SwiperSlide>
+            <img src={Skin1} alt='skin' className='skins' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Skin2} alt='skin' className='skins' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Skin3} alt='skin' className='skins' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Skin4} alt='skin' className='skins' />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={Skin5} alt='skin' className='skins' />
+          </SwiperSlide>
+        </Swiper>
       </div>
     </section>
   )
